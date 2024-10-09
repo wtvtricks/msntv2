9781b16d1df0f814f601d8fc072f55311aa1f30f
Pocket <114040611+PocketPii@users.noreply.github.com>
2024-10-09 06:14:56 -0400

    Fixed USB stuff

--- USB/main.py
+++ USB/main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, Response
 
 bootstrap = Flask(__name__)
 
 @bootstrap.route('/')
 def returnBootstrap():
-    return render_template("eval.html")
+    return Response("<script>window.location='msntv:/../hard disk/putonusb.html'</script>")
 
 if __name__ == '__main__':
     bootstrap.run(host='0.0.0.0', port=80)
